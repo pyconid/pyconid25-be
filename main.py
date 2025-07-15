306f460e4810027b3d87d24539bc2d884f5c0a73
Nizar Izzuddin Yatim Fadlan <hello@nizarfadlan.dev>
2025-07-15 23:33:56 +0700

    add SessionMiddleware to main.py for session management

--- main.py
+++ main.py
@@ -3,12 +3,22 @@ from fastapi.middleware.cors import CORSMiddleware
 from core.health_check import health_check
 from core.log import logger
 from routes.auth import router as auth_router
+from starlette.middleware.sessions import SessionMiddleware
+
+from settings import ENVIRONTMENT, SECRET_KEY
 
 health_check()
 
 
 app = FastAPI(title="PyconId 2025 BE")
 
+app.add_middleware(
+    SessionMiddleware,
+    secret_key=SECRET_KEY,
+    same_site="lax",
+    https_only=ENVIRONTMENT == "prod" or ENVIRONTMENT == "production",
+    max_age=1800,
+)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
