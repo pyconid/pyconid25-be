63172d9cd69b33f8ace2e68896ea5e87d6e3e776
maliqpotter09 <maliqpotter09@gmail.com>
2025-09-26 19:21:50 +0700

    fixing issue alembic head

--- migrations/versions/2025_09_26_1917-a513e39156cb_merge_google_id_and_ticket_seeder.py
+++ migrations/versions/2025_09_26_1917-a513e39156cb_merge_google_id_and_ticket_seeder.py
@@ -5,13 +5,13 @@ Revises: 1788b33307f3, seedticket0001
 Create Date: 2025-09-26 19:17:40.665481
 
 """
+
 from typing import Sequence, Union
 
 
-
 # revision identifiers, used by Alembic.
-revision: str = 'a513e39156cb'
-down_revision: Union[str, None] = ('1788b33307f3', 'seedticket0001')
+revision: str = "a513e39156cb"
+down_revision: Union[str, None] = ("1788b33307f3", "seedticket0001")
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
