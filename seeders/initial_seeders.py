68bd6aadd96af17b7660bf8c222bf9242ef829f5
Nizar Izzuddin Yatim Fadlan <hello@nizarfadlan.dev>
2025-11-25 23:17:38 +0700

    refactor: Comment out initial_country_city_state_seeders in initial_seeders function

--- seeders/initial_seeders.py
+++ seeders/initial_seeders.py
@@ -2,13 +2,13 @@ from seeders.initial_schedule_type import initial_schedule_type
 from seeders.initial_room import initial_room
 from models import factory_session
 
-from seeders.initial_country_city_state import initial_country_city_state_seeders
+# from seeders.initial_country_city_state import initial_country_city_state_seeders
 from seeders.initial_speaker_type import initial_speaker_type
 
 
 def initial_seeders():
     with factory_session() as session:
-        initial_country_city_state_seeders(db=session, is_commit=True)
+        # initial_country_city_state_seeders(db=session, is_commit=True)
         initial_speaker_type(db=session, is_commit=True)
         initial_room(db=session, is_commit=True)
         initial_schedule_type(db=session, is_commit=True)
