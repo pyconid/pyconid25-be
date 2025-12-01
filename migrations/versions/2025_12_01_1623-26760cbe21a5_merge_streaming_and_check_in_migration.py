cf43b3d61088f3d1e75643754074ce2221fae2f9
BimaAdi <bimaadi419@gmail.com>
2025-12-01 16:28:47 +0700

    fix ruff format

--- migrations/versions/2025_12_01_1623-26760cbe21a5_merge_streaming_and_check_in_migration.py
+++ migrations/versions/2025_12_01_1623-26760cbe21a5_merge_streaming_and_check_in_migration.py
@@ -9,7 +9,6 @@ Create Date: 2025-12-01 16:23:49.287566
 from typing import Sequence, Union
 
 
-
 # revision identifiers, used by Alembic.
 revision: str = "26760cbe21a5"
 down_revision: Union[str, None] = ("60bce827bab3", "643d2c34e927")
