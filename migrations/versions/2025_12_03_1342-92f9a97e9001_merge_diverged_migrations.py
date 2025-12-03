8564090bde8b976c04ab8daee66fe2009523e773
pradanaadn <pradanaadn21@gmail.com>
2025-12-03 13:46:24 +0800

    fix: correct formatting in migration file

--- migrations/versions/2025_12_03_1342-92f9a97e9001_merge_diverged_migrations.py
+++ migrations/versions/2025_12_03_1342-92f9a97e9001_merge_diverged_migrations.py
@@ -5,6 +5,7 @@ Revises: 6feac25c0745, 83852d14430b
 Create Date: 2025-12-03 13:42:39.739118
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op  # noqa: F401
@@ -12,8 +13,8 @@ import sqlalchemy as sa  # noqa: F401
 
 
 # revision identifiers, used by Alembic.
-revision: str = '92f9a97e9001'
-down_revision: Union[str, None] = ('6feac25c0745', '83852d14430b') # pyright: ignore[reportAssignmentType]
+revision: str = "92f9a97e9001"
+down_revision: Union[str, None] = ("6feac25c0745", "83852d14430b")  # pyright: ignore[reportAssignmentType]
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
