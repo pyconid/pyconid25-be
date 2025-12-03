bcf153e0d6774bac5d93d80ebbc1c5ac2df6cfa8
pradanaadn <pradanaadn21@gmail.com>
2025-12-03 13:44:38 +0800

    refactor: clean up formatting in migration file

--- migrations/versions/2025_12_03_1342-92f9a97e9001_merge_diverged_migrations.py
+++ migrations/versions/2025_12_03_1342-92f9a97e9001_merge_diverged_migrations.py
@@ -7,13 +7,13 @@ Create Date: 2025-12-03 13:42:39.739118
 """
 from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
+from alembic import op  # noqa: F401
+import sqlalchemy as sa  # noqa: F401
 
 
 # revision identifiers, used by Alembic.
 revision: str = '92f9a97e9001'
-down_revision: Union[str, None] = ('6feac25c0745', '83852d14430b')
+down_revision: Union[str, None] = ('6feac25c0745', '83852d14430b') # pyright: ignore[reportAssignmentType]
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
